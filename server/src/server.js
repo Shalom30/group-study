c65b8a79ccc06eec9d3558e0bc36ca634691fa0d
shalom30 <shalomwepngong23@gmail.com>
2026-06-17 11:36:30 +0100

    debugging host

--- server/src/server.js
+++ server/src/server.js
@@ -172,3 +172,7 @@ const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
   console.log('Server running on port', PORT)
 })
+
+app.get('/debug-env', (req, res) => {
+  res.json({ CLIENT_URL: process.env.CLIENT_URL })
+})
